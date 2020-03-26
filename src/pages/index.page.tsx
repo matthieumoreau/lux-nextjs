904a4aa1dfd5defab2fa22ea9c40c96de1f3e57e
Matthieu Moreau <dev@matthieumoreau.fr>
2020-03-27 00:38:58 +0100

    add styled-components

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,19 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
 
 import '../styles/index.less';
 interface Props {
   userAgent?: string;
 }
 
+const Title = styled.h1`
+  color: red;
+  font-size: 50px;
+`;
+
 const Page: NextPage<Props> = ({ userAgent }) => (
   <>
+    <Title>My page</Title>
     <div className="example">Hello World!</div>
     <main>Your user agent: {userAgent}</main>
   </>
